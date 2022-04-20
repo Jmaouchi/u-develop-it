1a3c5c619698ad6cf1068f5277dc08cd295c1e00
Jugurta Maouchi <djigo.maouchi@yahoo.com>
2022-04-19 19:18:27 -0500

    added  get, delete, put and update methodes to the parties data with creating diffirent API endpoints

--- server.js
+++ server.js
@@ -24,8 +24,9 @@ const db = mysql.createConnection(
 // this will return the data from your database and exactly from the condidates table
 // Get all candidates
 app.get('/api/candidates', (req, res) => {
-  // set a variable to get all the data from the candidates table 
-  const sql = `SELECT * FROM candidates`;
+  // set a variable to get all the data from the candidates table + the data that we gotten from the other table
+  const sql = `SELECT candidates.*, parties.name  AS party_name 
+             FROM candidates LEFT JOIN parties ON candidates.party_id = parties.id`;
 
   // get the data displayed as json using the db.query . note here we dont have a req.params due to us want to get all the data 
   db.query(sql, (err, rows) => {
@@ -45,8 +46,13 @@ app.get('/api/candidates', (req, res) => {
 
 // Get a single candidate
 app.get('/api/candidate/:id', (req, res) => {
-  // get the data from the database
-  const sql = `SELECT * FROM candidates WHERE id = ?`;
+  // get the data from the database ==> here we selected the database table and we added the party.name from the parties table then sent it
+  // whenever the id = user provided id then send the data that has that id. for exmpl http://localhost:3001/api/candidate/1 
+  //will provide them the data with first_name = Roland, last_name = Fribrank, party_id =1 ...+ Party_name = JS ..
+  const sql = `SELECT candidates.*, parties.name AS party_name 
+    FROM candidates 
+    LEFT JOIN parties ON candidates.party_id = parties.id
+    WHERE candidates.id = ?`;
   const params = [req.params.id];
 
   db.query(sql, params, (err, row) => {
@@ -114,6 +120,85 @@ app.post('/api/candidate', ({ body }, res) => {
   });
 });
 
+// get all parties
+app.get('/api/parties', (req, res) => {
+  const sql = `SELECT * FROM parties`;
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    });
+  });
+});
+
+
+// get a single party
+app.get('/api/party/:id', (req, res) => {
+  const sql = `SELECT * FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, row) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: row
+    });
+  });
+});
+
+
+// delete party
+app.delete('/api/party/:id', (req, res) => {
+  const sql = `DELETE FROM parties WHERE id = ?`;
+  const params = [req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: res.message });
+      // checks if anything was deleted
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Party not found'
+      });
+    } else {
+      res.json({
+        message: 'deleted',
+        changes: result.affectedRows,
+        id: req.params.id
+      });
+    }
+  });
+});
+
+
+
+// Update a candidate's party
+app.put('/api/candidate/:id', (req, res) => {
+  const sql = `UPDATE candidates SET party_id = ? 
+               WHERE id = ?`;
+  const params = [req.body.party_id, req.params.id];
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      // check if a record was found
+    } else if (!result.affectedRows) {
+      res.json({
+        message: 'Candidate not found'
+      });
+    } else {
+      res.json({
+        message: 'success',
+        data: req.body,
+        changes: result.affectedRows
+      });
+    }
+  });
+});
 
 // Default response for any other request (Not Found)
 // make sure that this need to be always last in your code 
